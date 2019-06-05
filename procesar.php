7c70b287c032dc6547c991f03dfcae490acadfdf
Israel_Jimenez <41273721+Erisjimver@users.noreply.github.com>
2019-06-05 10:59:29 -0500

    Add files via upload

---
+++ procesar.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>PHP</title>
+</head>
+<body>
+
+	<?php
+
+
+
+		print("Su nombre es: ".$_POST["nombre"]."<br>");
+		print("Su apellido es: ".$_POST["apellido"]."<br>");
+
+
+	?>
+
+</body>
+</html>
